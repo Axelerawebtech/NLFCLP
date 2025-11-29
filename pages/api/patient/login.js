4f9bc1fe649ca05ee266ccf67c56f46ce4ca0673
kalaibharath <kalaicbi28@gmail.com>
2025-11-29 18:26:43 +0530

    fix

--- pages/api/patient/login.js
+++ pages/api/patient/login.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../lib/mongodb';
-import Patient from '../../models/Patient';
+import dbConnect from '../../../lib/mongodb';
+import Patient from '../../../models/Patient';
 
 export default async function handler(req, res) {
   await dbConnect();
